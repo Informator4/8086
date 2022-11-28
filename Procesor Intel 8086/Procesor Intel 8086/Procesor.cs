5f74990d261bb8406fdd74fc6c1478eaf70058e1
Informator4 <matmoty22@gmail.com>
2022-11-28 23:40:20 +0100

    UPDATE 2
    Working command "MOV"

---
+++ Procesor Intel 8086/Procesor Intel 8086/Procesor.cs
@@ -0,0 +1,25 @@
+﻿public class Procesor
+{
+    // Rejestry
+    public short AX = 0;
+    public short BX = 0;
+    public short CX = 0;
+    public short DX = 0;
+
+    public int getAX()
+    {
+        return AX;
+    }
+    public int getBX()
+    {
+        return BX;
+    }
+    public int getCX()
+    {
+        return CX;
+    }
+    public int getDX()
+    {
+        return DX;
+    }
+}
--- Procesor Intel 8086/Procesor Intel 8086/Program.cs
+++ Procesor Intel 8086/Procesor Intel 8086/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        Procesor procesor = new Procesor();
         do
         {
             Console.Clear();
@@ -25,7 +28,7 @@ public class Program
                 Console.Clear();
                 Console.WriteLine("Commands:\n");
                 Console.WriteLine("Jak w Asemblerze..");
-                Thread.Sleep(2000);
+                Console.ReadKey();
             }
             else if (x == 3)
             {
@@ -40,22 +43,199 @@ public class Program
             }
 
 
-        } while (1 < 2);
-
-        // Rejestry
-        short AX = 0;
-        short BX = 0;
-        short CX = 0;
-        short DX = 0;
+        } while (true);
 
         do
         {
             Console.Clear();
 
-            Console.WriteLine("AX: {0}\nBX: {1}\nCX: {2}\nDX: {3}\n", AX, BX, CX, DX);
+            string a = "w g x", b;
+            short x;
 
-            int y = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("AX: {0}\nBX: {1}\nCX: {2}\nDX: {3}\n", procesor.getAX(), procesor.getBX(), procesor.getCX(), procesor.getDX());
+            Console.WriteLine("Enter the commend (MOV, ADD):");
 
-        }while (1 < 2);
+            a = Console.ReadLine();
+            string[] result = a.Split(" ");
+            
+
+            if (result[0] == "MOV") // MOV
+            {
+                if (short.TryParse(result[2], out x))
+                {
+                    if (result[1] == "AX,")
+                    {
+                        procesor.AX = x;
+                    }
+                    else if (result[1] == "BX,")
+                    {
+                        procesor.BX = x;
+                    }
+                    else if (result[1] == "CX,")
+                    {
+                        procesor.CX = x;
+                    }
+                    else if (result[1] == "DX,")
+                    {
+                        procesor.DX = x;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Err");
+                        Console.ReadKey();
+                    }
+                }
+                else if (!short.TryParse(result[2], out x)) // =-=-=-=-=-=-=-=-=-=-=-=-=
+                {
+
+                    if (result[1] == "AX,") // AX, ... ===
+                    {
+                        if (result[2] == "AX")
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                        else if (result[2] == "BX")
+                        {
+                            short i = procesor.AX;
+                            procesor.AX = procesor.BX;
+                            procesor.BX = i;
+                        }
+                        else if (result[2] == "CX")
+                        {
+                            short i = procesor.AX;
+                            procesor.AX = procesor.CX;
+                            procesor.CX = i;
+                        }
+                        else if (result[2] == "DX")
+                        {
+                            short i = procesor.AX;
+                            procesor.AX = procesor.DX;
+                            procesor.DX = i;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                    }
+                    else if (result[1] == "BX,") //BX, ... ===
+                    {
+                        if (result[2] == "AX")
+                        {
+                            short i = procesor.BX;
+                            procesor.BX = procesor.AX;
+                            procesor.AX = i;
+                        }
+                        else if (result[2] == "BX")
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                        else if (result[2] == "CX")
+                        {
+                            short i = procesor.BX;
+                            procesor.BX = procesor.CX;
+                            procesor.CX = i;
+                        }
+                        else if (result[2] == "DX")
+                        {
+                            short i = procesor.BX;
+                            procesor.BX = procesor.DX;
+                            procesor.DX = i;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                    }
+                    else if (result[1] == "CX,") //CX, ... ===
+                    {
+                        if (result[2] == "AX")
+                        {
+                            short i = procesor.CX;
+                            procesor.CX = procesor.AX;
+                            procesor.AX = i;
+                        }
+                        else if (result[2] == "BX")
+                        {
+                            short i = procesor.CX;
+                            procesor.CX = procesor.BX;
+                            procesor.BX = i;
+                        }
+                        else if (result[2] == "CX")
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                        else if (result[2] == "DX")
+                        {
+                            short i = procesor.CX;
+                            procesor.CX = procesor.DX;
+                            procesor.DX = i;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                    }
+                    else if (result[1] == "DX,") //DX, ... ===
+                    {
+                        if (result[2] == "AX")
+                        {
+                            short i = procesor.DX;
+                            procesor.DX = procesor.AX;
+                            procesor.AX = i;
+                        }
+                        else if (result[2] == "BX")
+                        {
+                            short i = procesor.DX;
+                            procesor.DX = procesor.BX;
+                            procesor.BX = i;
+                        }
+                        else if (result[2] == "CX")
+                        {
+                            short i = procesor.DX;
+                            procesor.DX = procesor.CX;
+                            procesor.CX = i;
+                        }
+                        else if (result[2] == "DX")
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Err");
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Err");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Err");
+                    Console.ReadKey();
+                }
+            }
+            else if(result[0] == "ADD") // ADD
+            {
+                if (result[1] == "AX,")
+                {
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Err");
+                Console.ReadKey();
+            }
+        } while (true);
     }
 }
